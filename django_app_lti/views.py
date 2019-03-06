81726f72eb55729761da8f34c837c029b2e6ea85
Thomas Mcleod <td_mcleod@Thomass-MBP.modem>
2019-03-06 22:14:52 +1100

    updated breaking changes to urlresolver

--- django_app_lti/views.py
+++ django_app_lti/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.auth import logout
 from django.views.generic import View
 from django.conf import settings
